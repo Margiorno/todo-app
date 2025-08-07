9394aec9f606311e8298ba517cb73cf4aa4934d3
Margiorno <m.zawiski03@gmail.com>
2025-08-07 15:18:53 +0200

    update profile picture script update

--- todo-app/src/main/java/com/pm/todoapp/controller/UserController.java
+++ todo-app/src/main/java/com/pm/todoapp/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -22,11 +24,11 @@ public class UserController {
     }
 
     @PostMapping("/profile-picture")
-    public ResponseEntity<String> uploadProfilePicture(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal UUID userId) {
+    public ResponseEntity<Map<String,String>> uploadProfilePicture(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal UUID userId) {
 
         String filename = usersService.updateProfilePicture(file, userId);
 
-        return ResponseEntity.ok("Profile picture uploaded successfully: " + filename);
+        return ResponseEntity.ok(Collections.singletonMap("filename", filename));
     }
 
 

30ea1743c1070d9f5f38f2a84f53b42bda321a67
Margiorno <m.zawiski03@gmail.com>
2025-09-06 18:40:58 +0200

    notification listener tested

--- todo-app/integration-tests/src/test/java/com/pm/todoapp/notifications/UserNotificationsListenerTest.java
+++ todo-app/integration-tests/src/test/java/com/pm/todoapp/notifications/UserNotificationsListenerTest.java
@@ -1,21 +1,17 @@
 package com.pm.todoapp.notifications;
 
+import com.pm.todoapp.domain.user.event.FriendRequestAcceptedEvent;
+import com.pm.todoapp.domain.user.event.FriendRequestResolvedEvent;
 import com.pm.todoapp.domain.user.event.FriendRequestSentEvent;
-import com.pm.todoapp.notifications.listener.UserNotificationsListener;
 import com.pm.todoapp.notifications.service.NotificationService;
 import org.instancio.Instancio;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.transaction.AfterTransaction;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -45,5 +41,31 @@ public class UserNotificationsListenerTest {
                 event.senderId(),
                 event.receiverId()
         );
+    }
+
+    @Test
+    public void shouldHandleFriendRequestAcceptedEvent() {
+        FriendRequestAcceptedEvent event = Instancio.create(FriendRequestAcceptedEvent.class);
+
+        transactionTemplate.execute(status->{
+            eventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(notificationService, times(1)).createFriendRequestAcceptedNotification(
+                event.acceptorId(), event.senderId()
+        );
+    }
+
+    @Test
+    public void shouldHandleFriendRequestResolvedEvent() {
+        FriendRequestResolvedEvent event = Instancio.create(FriendRequestResolvedEvent.class);
+
+        transactionTemplate.execute(status->{
+            eventPublisher.publishEvent(event);
+            return null;
+        });
+
+        verify(notificationService, times(1)).resolveNotification(event.requestId());
     }
 }

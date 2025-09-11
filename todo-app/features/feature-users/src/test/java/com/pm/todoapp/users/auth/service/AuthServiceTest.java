99ad12cf1f7898821c9ab6b09a83c48b026dad4c
Margiorno <m.zawiski03@gmail.com>
2025-09-11 20:26:16 +0200

    Auth service test pt 1

---
+++ todo-app/features/feature-users/src/test/java/com/pm/todoapp/users/auth/service/AuthServiceTest.java
@@ -0,0 +1,64 @@
+package com.pm.todoapp.users.auth.service;
+
+import com.pm.todoapp.common.security.JwtUtil;
+import com.pm.todoapp.users.auth.dto.RegisterRequestDTO;
+import com.pm.todoapp.users.profile.model.User;
+import com.pm.todoapp.users.profile.service.UsersService;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    private UsersService usersService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    void registerUser_shouldSaveUserAndReturnToken_whenEmailIsUnique() {
+        RegisterRequestDTO registerRequest = Instancio.create(RegisterRequestDTO.class);
+        String hashedPassword = Instancio.create(String.class);
+        String generatedToken = Instancio.create(String.class);
+        User savedUser = Instancio.create(User.class);
+
+        when(usersService.existByEmail(registerRequest.getEmail())).thenReturn(false);
+        when(passwordEncoder.encode(registerRequest.getPassword())).thenReturn(hashedPassword);
+        when(usersService.save(any(User.class))).thenReturn(savedUser);
+        when(jwtUtil.generateToken(savedUser.getId().toString())).thenReturn(generatedToken);
+
+        String resultToken = authService.registerUser(registerRequest);
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(usersService).save(userCaptor.capture());
+        User capturedUser = userCaptor.getValue();
+
+        assertThat(capturedUser.getEmail()).isEqualTo(registerRequest.getEmail());
+        assertThat(capturedUser.getFirstName()).isEqualTo(registerRequest.getFirstName());
+        assertThat(capturedUser.getPassword()).isEqualTo(hashedPassword);
+
+        verify(usersService).existByEmail(registerRequest.getEmail());
+        verify(passwordEncoder).encode(registerRequest.getPassword());
+        verify(jwtUtil).generateToken(savedUser.getId().toString());
+
+        assertThat(resultToken).isEqualTo(generatedToken);
+    }
+
+}

38b4f2368ae4fb6dfc65b25e3abec317f8c8a53f
Margiorno <m.zawiski03@gmail.com>
2025-09-11 15:04:41 +0200

    Team service test pt 1

---
+++ todo-app/features/feature-teams/src/test/java/com/pm/todoapp/teams/service/TeamServiceTest.java
@@ -0,0 +1,70 @@
+package com.pm.todoapp.teams.service;
+
+import com.pm.todoapp.common.exceptions.InvalidTeamInviteException;
+import com.pm.todoapp.common.exceptions.TeamNotFoundException;
+import com.pm.todoapp.domain.user.dto.UserDTO;
+import com.pm.todoapp.domain.user.model.User;
+import com.pm.todoapp.domain.user.port.UserProviderPort;
+import com.pm.todoapp.domain.user.port.UserValidationPort;
+import com.pm.todoapp.domain.user.repository.UserRepository;
+import com.pm.todoapp.teams.dto.TeamInviteResponseDTO;
+import com.pm.todoapp.teams.dto.TeamMemberDTO;
+import com.pm.todoapp.teams.dto.TeamResponseDTO;
+import com.pm.todoapp.teams.model.Invite;
+import com.pm.todoapp.teams.model.Team;
+import com.pm.todoapp.teams.repository.TeamRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.instancio.Select.field;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TeamServiceTest {
+
+    @Mock
+    private TeamRepository teamRepository;
+    @Mock
+    private UserValidationPort userValidationPort;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserProviderPort userProviderPort;
+    @Mock
+    private InviteCodeService inviteCodeService;
+
+    @InjectMocks
+    private TeamService teamService;
+
+    private User user;
+    private Team team;
+
+    @BeforeEach
+    void setUp() {
+        user = Instancio.create(User.class);
+        team = Instancio.of(Team.class)
+                .set(field(Team::getMembers),Set.of(user))
+                .create();
+    }
+
+    @Test
+    void findRawById_shouldThrowException_whenIdIsNull() {
+        assertThatThrownBy(() -> teamService.findRawById(null))
+                .isInstanceOf(TeamNotFoundException.class)
+                .hasMessage("Team not found");
+    }
+
+
+}

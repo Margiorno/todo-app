ebcb932ded1c09f7bf4b5613f523ca2155c7a943
Margiorno <m.zawiski03@gmail.com>
2025-09-11 20:53:54 +0200

    UsersService test pt 3 (update profile picture)

--- todo-app/features/feature-users/src/test/java/com/pm/todoapp/users/profile/service/UsersServiceTest.java
+++ todo-app/features/feature-users/src/test/java/com/pm/todoapp/users/profile/service/UsersServiceTest.java
@@ -1,6 +1,8 @@
 package com.pm.todoapp.users.profile.service;
 
+import com.pm.todoapp.common.exceptions.StorageException;
 import com.pm.todoapp.common.exceptions.UserNotFoundException;
+import com.pm.todoapp.domain.file.dto.FileType;
 import com.pm.todoapp.domain.file.port.FileStoragePort;
 import com.pm.todoapp.users.profile.model.User;
 import com.pm.todoapp.users.profile.repository.UsersRepository;
@@ -8,10 +10,15 @@ import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -80,6 +87,44 @@ public class UsersServiceTest {
         assertThatThrownBy(() -> usersService.findByEmail(user.getEmail()))
                 .isInstanceOf(UserNotFoundException.class)
                 .hasMessage("User with this email does not exist: " + user.getEmail());
+    }
+
+    @Test
+    void updateProfilePicture_shouldSaveNewPictureAndDeleteOldOne() throws IOException {
+        String oldPicturePath = Instancio.create(String.class);
+        String newPicturePath = Instancio.create(String.class);
+        user.setProfilePicturePath(oldPicturePath);
+        MockMultipartFile file = Instancio.create(MockMultipartFile.class);
+
+        when(usersRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(fileStoragePort.saveFile(any(InputStream.class), eq(file.getOriginalFilename()), eq(FileType.PROFILE_PICTURE)))
+                .thenReturn(newPicturePath);
+
+        String resultPath = usersService.updateProfilePicture(file, user.getId());
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(usersRepository).save(userCaptor.capture());
+        User savedUser = userCaptor.getValue();
+
+        assertThat(resultPath).isEqualTo(newPicturePath);
+        assertThat(savedUser.getProfilePicturePath()).isEqualTo(newPicturePath);
+
+        verify(fileStoragePort).saveFile(any(InputStream.class), eq(file.getOriginalFilename()), eq(FileType.PROFILE_PICTURE));
+        verify(fileStoragePort).deleteFile(oldPicturePath, FileType.PROFILE_PICTURE);
+    }
+
+    @Test
+    void updateProfilePicture_shouldThrowStorageException_whenFileReadFails() throws IOException {
+        MultipartFile file = mock(MultipartFile.class);
+        when(file.getInputStream()).thenThrow(new IOException("Test exception"));
+        when(usersRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        assertThatThrownBy(() -> usersService.updateProfilePicture(file, user.getId()))
+                .isInstanceOf(StorageException.class)
+                .hasMessage("Failed to read uploaded profile picture for user: " + user.getId());
+
+        verify(usersRepository, never()).save(any(User.class));
+        verify(fileStoragePort, never()).deleteFile(anyString(), any(FileType.class));
     }
 
 }

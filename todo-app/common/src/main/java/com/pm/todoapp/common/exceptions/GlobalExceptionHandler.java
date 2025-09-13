facd25de55ad6420ceaec0799612cefba2afa365
Margiorno <m.zawiski03@gmail.com>
2025-09-13 10:50:34 +0200

    tasks exceptions handled

--- todo-app/common/src/main/java/com/pm/todoapp/common/exceptions/GlobalExceptionHandler.java
+++ todo-app/common/src/main/java/com/pm/todoapp/common/exceptions/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.pm.todoapp.common.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -15,7 +17,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({
             ConversationNotFoundException.class,
             StorageFileNotFoundException.class,
-            NotificationNotFoundException.class
+            NotificationNotFoundException.class,
+            TaskNotFoundException.class,
     })
     public ResponseEntity<Map<String,String>> handleNotFoundStatusExceptions(ConversationNotFoundException ex) {
         String message = ex.getMessage();
@@ -41,6 +44,36 @@ public class GlobalExceptionHandler {
         map.put("message", message);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+    }
+
+    @ExceptionHandler(TaskAccessDeniedException.class)
+    public ResponseEntity<Map<String,String>> handleTaskAccessDeniedException(TaskAccessDeniedException ex) {
+        String message = ex.getMessage();
+        Map<String,String> map = new HashMap<>();
+        map.put("message", message);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(map);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Validation failed");
+
+        List<Map<String, String>> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> err = new HashMap<>();
+                    err.put("field", error.getField());
+                    err.put("error", error.getDefaultMessage());
+                    return err;
+                })
+                .toList();
+
+        body.put("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
 }
